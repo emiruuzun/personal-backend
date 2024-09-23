88f5081d2c4c757bae279b41bf591c0cc0d07d7b
emiruuzun <emirsah.41@gmail.com>
2024-09-23 14:15:56 +0300

    fix

--- helpers/database/cronJob.js
+++ helpers/database/cronJob.js
@@ -1,28 +1,46 @@
 const { CronJob } = require("cron");
 const User = require("../../models/User");
+const Leave = require("../../models/LeaveRequest");
 
 const startStatusUpdateJob = () => {
   const job = new CronJob("0 0 * * *", async () => {
-    // Her gece 00:00'da çalışacak
     try {
-      const today = new Date().toISOString().split("T")[0];
+      // Bugünün yerel tarihini al ve saatlerini sıfırla
+      const today = new Date();
+      console.log(today);
+      // Leave tablosundan endDate bugünden önce olan izinleri bul
+      const expiredLeaves = await Leave.find({
+        endDate: { $lt: today },
+        status: "Onaylandı",
+      });
 
-      const result = await User.updateMany(
-        { leaveEndDate: { $lt: today }, status: "İzinli" },
-        { $set: { status: "Aktif" } }
-      );
+      console.log(expiredLeaves);
 
-      if (result.nModified > 0) {
-        console.log(
-          `${result.nModified} kullanıcının statüsü "Aktif" olarak güncellendi.`
+      if (expiredLeaves.length > 0) {
+        const userIdsToUpdate = expiredLeaves.map((leave) => leave.userId);
+
+        // Bu kullanıcıların durumunu Aktif olarak güncelle
+        const result = await User.updateMany(
+          { _id: { $in: userIdsToUpdate }, status: "İzinli" },
+          { $set: { status: "Aktif" } }
         );
+
+        console.log(result);
+        if (result.modifiedCount > 0) {
+          console.log(
+            `${result.modifiedCount} kullanıcının statüsü "Aktif" olarak güncellendi.`
+          );
+        } else {
+          console.log("Güncellenmesi gereken kullanıcı bulunamadı.");
+        }
       } else {
-        console.log("Güncellenmesi gereken kullanıcı bulunamadı.");
+        console.log("Süresi dolmuş izin kaydı bulunamadı.");
       }
     } catch (error) {
       console.error("Bir hata oluştu:", error);
     }
   });
+
   job.start();
 };
 
