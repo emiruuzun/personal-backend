0ef2304eebe8aaf279ea4c0603011c33abb2eb2a
emiruuzun <emirsah.41@gmail.com>
2024-11-25 11:28:31 +0300

    fix

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ app.use((req, res, next) => {
 
 // Routers Middleware should be placed after the io middleware
 app.use("/", routers);
-console.log("mr.");
+console.log("mrb");
 
 // Custom Error Handler
 app.use(customErrorHandler);
