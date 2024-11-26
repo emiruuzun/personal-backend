7d8a55382ff78212611c5d611644c1916fe88ff3
emiruuzun <emirsah.41@gmail.com>
2024-11-26 11:31:02 +0300

    fix jwt token old

--- helpers/authorization/tokenHelpers.js
+++ helpers/authorization/tokenHelpers.js
@@ -9,7 +9,7 @@ const sendJwtToClient = (user, res)=>{
     .status(200)
     .cookie("access_token",token, {
 
-        httpOnly :false,
+        httpOnly :true,
         expires: new Date(Date.now() + parseInt(JWT_COOKIE) *  1000),
         secure : NODE_ENV === "development" ? false : true
     })
